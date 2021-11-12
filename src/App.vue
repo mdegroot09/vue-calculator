0cc375330f49ce7cf9bff913bf976b75f361ce60
mdegroot09 <mdegroot09@gmail.com>
2021-11-12 13:55:42 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Calculator 
+    title="I'm a title"
+    div="I'm a div"
+    />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Calculator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="hello">
+        {{title}}
+        <div>
+            {{div}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+  props: {
+      title: String,
+      div: String
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+    background: lightblue;
+}
+</style>
