0bc7a24af5be958151845d2a824896e323e09589
mdegroot09 <mdegroot09@gmail.com>
2021-11-15 14:53:12 -0700

    Updated styling and button designations

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,28 +1,28 @@
 <template>
     <div class="w-screen h-screen grid items-center justify-center">
         <div class="calculator rounded-md bg-blue-400 grid grid-cols-1 grid-rows-2 items-center justify-center">
-            <div class="screen bg-gray-100 rounded-md"></div>
+            <div class="screen bg-yellow-100 rounded-md"></div>
             <div class="calcBtns h-full grid grid-cols-4 grid-rows-5 gap-3">
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">(</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">)</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center" style="paddingTop: 3px">^</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">C</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">7</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">8</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">9</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">/</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">4</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">5</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">6</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">X</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">1</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">2</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">3</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">-</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">0</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">.</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">+</div></div>
-                <div class="w-full bg-gray-100 rounded-md grid items-center"><div class="btnText text-center">=</div></div>
+                <div class="cursor-pointer w-full bg-gray-500 rounded-md grid items-center"><div class="btnText text-center">(</div></div>
+                <div class="cursor-pointer w-full bg-gray-500 rounded-md grid items-center"><div class="btnText text-center">)</div></div>
+                <div class="cursor-pointer w-full bg-gray-500 rounded-md grid items-center"><div class="btnText text-center" style="paddingTop: 6px">^</div></div>
+                <div class="cursor-pointer w-full bg-gray-500 rounded-md grid items-center"><div class="btnText text-center">/</div></div>
+                <div class="cursor-pointer w-full bg-gray-300 rounded-md grid items-center"><div class="btnText text-center">7</div></div>
+                <div class="cursor-pointer w-full bg-gray-300 rounded-md grid items-center"><div class="btnText text-center">8</div></div>
+                <div class="cursor-pointer w-full bg-gray-300 rounded-md grid items-center"><div class="btnText text-center">9</div></div>
+                <div class="cursor-pointer w-full bg-gray-500 rounded-md grid items-center"><div class="btnText text-center">x</div></div>
+                <div class="cursor-pointer w-full bg-gray-300 rounded-md grid items-center"><div class="btnText text-center">4</div></div>
+                <div class="cursor-pointer w-full bg-gray-300 rounded-md grid items-center"><div class="btnText text-center">5</div></div>
+                <div class="cursor-pointer w-full bg-gray-300 rounded-md grid items-center"><div class="btnText text-center">6</div></div>
+                <div class="cursor-pointer w-full bg-gray-500 rounded-md grid items-center"><div class="btnText text-center">-</div></div>
+                <div class="cursor-pointer w-full bg-gray-300 rounded-md grid items-center"><div class="btnText text-center">1</div></div>
+                <div class="cursor-pointer w-full bg-gray-300 rounded-md grid items-center"><div class="btnText text-center">2</div></div>
+                <div class="cursor-pointer w-full bg-gray-300 rounded-md grid items-center"><div class="btnText text-center">3</div></div>
+                <div class="cursor-pointer w-full bg-gray-500 rounded-md grid items-center"><div class="btnText text-center">+</div></div>
+                <div class="cursor-pointer w-full bg-red-400 rounded-md grid items-center"><div class="btnText text-center">C</div></div>
+                <div class="cursor-pointer w-full bg-gray-300 rounded-md grid items-center"><div class="btnText text-center">0</div></div>
+                <div class="cursor-pointer w-full bg-gray-300 rounded-md grid items-center"><div class="btnText text-center">.</div></div>
+                <div class="cursor-pointer w-full bg-yellow-500 rounded-md grid items-center"><div class="btnText text-center">=</div></div>
             </div>
         </div>
     </div>
@@ -54,7 +54,8 @@ export default {
     padding: 40px;
 }
 .btnText {
-    height: 100%;
     vertical-align: middle;
+    font-weight: 700;
+    font-size: 1.3em;
 }
 </style>
