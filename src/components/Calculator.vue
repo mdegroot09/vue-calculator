86a062adb4ef4b90055d75cff04b4a5c1bd60f61
mdegroot09 <mdegroot09@gmail.com>
2021-11-22 15:27:33 -0700

    Added handling of button clicks and equation solving

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,30 +1,33 @@
 <template>
     <div class="w-screen h-screen grid items-center justify-center">
-        <div class="calculator rounded-md bg-blue-400 grid grid-cols-1 grid-rows-2 items-center justify-center">
+        <div class="calculator rounded-md bg-blue-400 grid grid-cols-1 grid-rows-3 items-center justify-center">
             <div class="screen bg-yellow-100 rounded-md grid justify-end items-center">
-                <span class="screenText overflow-x-auto overflow-y-hidden text-4xl font-medium">{{screenText}}</span>
+                <span class="margin-right-15px overflow-x-auto overflow-y-hidden text-5xl font-medium">{{screen1Num}}</span>
+            </div> 
+            <div class="screen screen2 bg-gray-300 rounded-md grid justify-end items-center self-start">
+                <span class="margin-right-15px overflow-x-auto overflow-y-hidden text-xl font-medium">{{equation}}</span>
             </div> 
             <div class="calcBtns h-full grid grid-cols-4 grid-rows-5 gap-3">
-                <button class="calcBtn cursor-pointer w-full bg-gray-500 rounded-md grid items-center" @click="increment()">(</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-500 rounded-md grid items-center" @click="increment()">)</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-500 rounded-md grid items-center" @click="increment()">^</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-500 rounded-md grid items-center" @click="increment()">/</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="increment()">7</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="increment()">8</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="increment()">9</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-500 rounded-md grid items-center" @click="increment()">x</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="increment()">4</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="increment()">5</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="increment()">6</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-500 rounded-md grid items-center" @click="increment()">-</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="increment()">1</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="increment()">2</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="increment()">3</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-500 rounded-md grid items-center" @click="increment()">+</button>
-                <button class="calcBtn cursor-pointer w-full bg-red-400 rounded-md grid items-center" @click="increment()">C</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="increment()">0</button>
-                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="increment()">.</button>
-                <button class="calcBtn cursor-pointer w-full bg-yellow-500 rounded-md grid items-center" @click="increment()">=</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-500 rounded-md grid items-center" @click="updateEquation('(')">(</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-500 rounded-md grid items-center" @click="updateEquation(')')">)</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-500 rounded-md grid items-center" @click="updateEquation('^')">^</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-500 rounded-md grid items-center" @click="updateEquation('/')">/</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="updateEquation(7)">7</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="updateEquation(8)">8</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="updateEquation(9)">9</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-500 rounded-md grid items-center" @click="updateEquation('*')">x</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="updateEquation(4)">4</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="updateEquation(5)">5</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="updateEquation(6)">6</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-500 rounded-md grid items-center" @click="updateEquation('-')">-</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="updateEquation(1)">1</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="updateEquation(2)">2</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="updateEquation(3)">3</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-500 rounded-md grid items-center" @click="updateEquation('+')">+</button>
+                <button class="calcBtn cursor-pointer w-full bg-red-400 rounded-md grid items-center" @click="updateEquation('C')">C</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="updateEquation(0)">0</button>
+                <button class="calcBtn cursor-pointer w-full bg-gray-300 rounded-md grid items-center" @click="updateEquation('.')">.</button>
+                <button class="calcBtn cursor-pointer w-full bg-yellow-500 rounded-md grid items-center" @click="updateEquation('=')">=</button>
             </div>
         </div>
     </div>
@@ -40,12 +43,15 @@ export default {
         div: String
     },
     methods: {
-        increment(){
-            this.$store.commit('increment')
+        updateEquation(val){
+            this.$store.commit('updateEquation', {
+                val
+            })
         }
     },
     computed: mapState({
-        screenText: state => state.screenText
+        screen1Num: state => state.screen1Num,
+        equation: state => state.equation
     }),
 }
 </script>)
@@ -56,16 +62,20 @@ export default {
     min-height: 400px;
     width: calc(100vw - 50px);
     max-width: calc(100vh * .75);
-    grid-template-rows: 150px 1fr;
+    grid-template-rows: auto auto 1fr;
 }
 .screen {
     height: 70px;
     margin: 40px;
+    margin-bottom: 15px;
     vertical-align: center;
 }
-.screenText {
+.margin-right-15px {
     margin-right: 15px;
-
+}
+.screen2 {
+    margin: 0 40px;
+    height: 30px;
 }
 .calcBtns {
     padding: 40px;
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,103 @@
 import {createStore} from 'vuex'
+import {evaluate} from 'mathjs'
 
 const store = createStore({
     state: {
-        screenText: 0
+        screen1Num: 0,
+        equation: '',
+        appendToScreenNum: false,
+        readyForNum: true,
+        readyForOperation: false,
+        appendString: '',
+        openParenthesisCount: 0
     },
     mutations: {
-        increment: state => state.screenText++,
-        decrement: state => state.screenText--
+        updateEquation(state, payload){
+
+            // handle numbers
+            if (typeof(payload.val) === 'number' && state.readyForNum){
+                // don't allow zero as beginning number
+                if(!state.appendToScreenNum && payload.val === 0){
+                    return 
+                }
+                // if beginning of equation
+                else if (state.screen1Num === 0 && !state.equation){
+                    state.screen1Num = payload.val
+                    state.equation = String(payload.val)
+                }
+                // if appending to a number
+                else if(state.appendToScreenNum){
+                    state.screen1Num = Number(`${state.screen1Num}${payload.val}`)
+                    state.equation += String(payload.val)
+                }
+                // if ready for a number
+                else {
+                    state.screen1Num = payload.val;
+                    state.equation += state.appendString + String(payload.val)
+                }
+                // update settings
+                state.appendToScreenNum = true
+                state.readyForNum = true
+                state.readyForOperation = true
+                state.appendString = ''
+                return
+            }
+
+            else if(payload.val === '('){
+                state.equation += state.appendString + '('
+                state.appendToScreenNum = false
+                state.readyForNum = true
+                state.readyForOperation = false
+                state.appendString = ''
+                state.openParenthesisCount++
+                return
+            }
+
+            // closed parenthesis must have open parenthesis
+            else if(payload.val === ')' && state.openParenthesisCount > 0){
+                state.equation += ')'
+                state.appendToScreenNum = false
+                state.readyForNum = false
+                state.readyForOperation = true
+                state.appendString = ' '
+                state.openParenthesisCount--
+                return
+            }
+
+            // handle operations
+            else if((payload.val === '^' || payload.val === '*' || payload.val === '/' || payload.val === '+' || payload.val === '-') && state.readyForOperation){
+                state.equation += ` ${payload.val}`
+                state.appendToScreenNum = false
+                state.readyForNum = true
+                state.readyForOperation = false
+                state.appendString = ' '
+                return
+            }
+
+            // handle clear
+            else if (payload.val === 'C'){
+                state.screen1Num = 0,
+                state.equation = '',
+                state.appendToScreenNum = false,
+                state.readyForNum = true,
+                state.readyForOperation = false,
+                state.appendString = '',
+                state.openParenthesisCount = 0
+                return
+            }
+
+            else if (payload.val === '='){
+                try{
+                    let solution = evaluate(state.equation)
+                    if(typeof(solution) === 'number'){
+                        state.screen1Num = solution
+                    }
+                }
+                catch(e) {
+                    return
+                }
+            }
+        }
     }
 })
   
